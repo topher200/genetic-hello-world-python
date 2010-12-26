6200375175fa9f24750a2a7370c3d7e8b2bddd97
Topher Brown <topher200@gmail.com>
2010-12-26 03:19:27 -0500

    added doctests

--- genetic-hello-world.py
+++ genetic-hello-world.py
@@ -6,6 +6,21 @@ class GeneticHelloWorld():
   def fitness(self, chromo):
     ''' Fitness of a chromo is the sum of how far each of its charactors is
     from the target string.
+
+    A string that exactly matched the target string has a fitness of 0. Lower
+    fitness is better than higher.
+
+    Tests assume that 'Hello World!' is the target string.
+    TODO(topher): this restriction could be removed
+    >>> fitness('Hello World!')
+    0
+    >>> fitness('Iello World!')
+    1
+    >>> fitness('Iallo World!')
+    5
+    >>> fitness('Hello!')
+    Traceback (most recent call last):
+    except ValueError: input chromo length doesn't match target
     '''
     pass
 
