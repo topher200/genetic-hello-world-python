a03209a72b689b3b82a1b652d5e25c60394decd3
Topher Brown <topher200@gmail.com>
2010-12-26 02:55:07 -0500

    initial commit

---
+++ genetic-hello-world.py
@@ -0,0 +1,19 @@
+class GeneticHelloWorld():
+  def generate_random_chromosomes(self):
+    pass
+
+
+  def fitness(self, chromo):
+    pass
+
+
+  def tourny_select_chromo(self):
+    pass
+
+
+  def breed(self, a, b):
+    pass
+
+
+  def mutate(self, chromo):
+    pass
