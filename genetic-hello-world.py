30ef4782d5ad7d07470a780d7f9641a9c8614287
Topher Brown <topher200@gmail.com>
2010-12-31 13:50:34 -0500

    removing the check in fitness() for input len(string) == len(TARGET). Added a comment saying that the lengths should match
    I didn't want this check in the doctests anymore (it seemed to be cluttering), and I don't think the multi line check at the start of the function was necessary either. If this fails it should be clear what the problem is. Besides, we're creating our own chromos, so there shouldn't be any situation that they would not be same length as target.

--- genetic-hello-world.py
+++ genetic-hello-world.py
@@ -23,8 +23,9 @@ def fitness(chromo):
   ''' Fitness of a chromo is the sum of how far each of its charactors is
   from the target string.
 
-  A string that exactly matched the target string has a fitness of 0. Lower
-  fitness is better than higher.
+  Input string must be the same length as the TARGET string. A string that
+  exactly matches the TARGET string has a fitness of 0. Lower fitness is
+  better than higher.
 
   Tests assume that 'Hello World!' is the target string.
   >>> fitness('Hello World!')
@@ -33,15 +34,7 @@ def fitness(chromo):
   1
   >>> fitness('Iallo World!')
   5
-  >>> fitness('Hello!')
-  Traceback (most recent call last):
-    ...
-  ValueError: input chromo length doesn't match target
   '''
-  if len(chromo) != len(TARGET):
-    print chromo
-    raise ValueError("input chromo length doesn't match target")
-
   total_fitness = 0
   for i, char in enumerate(chromo):
     total_fitness += abs(ord(char) - ord(TARGET[i]))
